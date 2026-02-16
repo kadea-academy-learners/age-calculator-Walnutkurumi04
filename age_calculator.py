519a75ef3ab53d29b53ed74f3cd88f8a985de306
Dan Leya <dleyatoko04@gmail.com>
2026-02-16 15:52:25 +0100

    1er essai

--- age_calculator.py
+++ age_calculator.py
@@ -1,11 +1,9 @@
 import datetime
 
 def main():
-    birth_year = 0  
-
-    current_year = 0 
-
-    age = 0
+    birth_year = int(input("Enter birth year: "))
+    current_year = datetime.datetime.now().year
+    age = current_year - birth_year
 
     print("--------------------")
     print(f"Current Year: {current_year}")
